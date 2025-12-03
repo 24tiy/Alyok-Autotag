bb66386bf3c928685bd1254db353bcd5e79cb0ae
Alexander Fokin <77535903+24tiy@users.noreply.github.com>
2025-12-03 21:47:06 +0100

    Update obsidian-guards.ts

--- src/utils/obsidian-guards.ts
+++ src/utils/obsidian-guards.ts
@@ -1,9 +1,9 @@
-import { App, TAbstractFile, TFile, TFolder, normalizePath } from "obsidian";
+import { App, TFile, TFolder, normalizePath } from "obsidian";
 
-export const isTFile = (x: TAbstractFile | null | undefined): x is TFile =>
+export const isTFile = (x: unknown): x is TFile =>
   x instanceof TFile;
 
-export const isTFolder = (x: TAbstractFile | null | undefined): x is TFolder =>
+export const isTFolder = (x: unknown): x is TFolder =>
   x instanceof TFolder;
 
 export function mustGetFile(app: App, path: string): TFile {
@@ -11,6 +11,7 @@ export function mustGetFile(app: App, path: string): TFile {
   if (!(af instanceof TFile)) throw new Error(`Not a file: ${path}`);
   return af;
 }
+
 export function mustGetFolder(app: App, path: string): TFolder {
   const af = app.vault.getAbstractFileByPath(normalizePath(path));
   if (!(af instanceof TFolder)) throw new Error(`Not a folder: ${path}`);
